703b433cc7c0cd51132d185eccea3adf386e9fb0
Christopher Leja <christopher.leja@gmail.com>
2020-01-31 21:12:34 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -23,31 +23,3 @@ def print_first_directors_movie_titles
   end
   spielberg_movies
 end
-
-#directors_database = { name is hash 1, movies is an array, title, studio, worldwide_gross and release_year are hash pairs
-#:name=>"Stephen Spielberg", 
-#:movies=>[
-  #{:title=>"Jaws", 
-    #:studio=>"Universal", 
-    #:worldwide_gross=>260000000, 
-    #:release_year=>1975}, 
-  #{:title=>"Close Encounters of the Third Kind", 
-    #:studio=>"Columbia", 
-    #:worldwide_gross=>135189114, 
-    #:release_year=>1977}, 
-  #{:title=>"Raiders of the Lost Ark", 
-    #:studio=>"Paramount", 
-    #:worldwide_gross=>248159971, 
-    #:release_year=>1981}, 
- #{:title=>"E.T. the Extra-terrestrial", 
-    #:studio=>"Universal", 
-    #:worldwide_gross=>435110554, 
-    #:release_year=>1982},
-  #{:title=>"Schindler's List", 
-    #:studio=>"Universal", 
-    #:worldwide_gross=>96898818, 
-    #:release_year=>1993}, 
-  #{:title=>"Lincoln", 
-    #:studio=>"Buena Vista", 
-    #:worldwide_gross=>182207973, 
-    #:release_year=>2012}]}
