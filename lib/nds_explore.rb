5294cc0850855771a23583564c0aa0009d79c540
Christopher Leja <christopher.leja@gmail.com>
2020-01-31 18:57:41 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -18,7 +18,7 @@ def print_first_directors_movie_titles
     column_index = 0
     while column_index < directors_database[row_index].length do
     #  if directors_database[row_index][:name][:movies][:title] 
-      puts "#{directors_database[0][:name][]}"
+      puts "#{directors_database[0][:title][column_index]}"
      #end 
       inner_index = 0
       while inner_index < directors_database[row_index[column_index]].length do
