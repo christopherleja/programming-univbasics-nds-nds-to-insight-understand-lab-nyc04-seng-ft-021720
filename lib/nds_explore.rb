6fcc09d4b98137ce07720dffbf9de597ec1bf920
Christopher Leja <christopher.leja@gmail.com>
2020-01-31 20:15:33 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -21,7 +21,7 @@ def print_first_directors_movie_titles
       #puts directors_database[0][:title]
       #end 
       inner_index = 0
-      while inner_index < directors_database[row_index].length do
+      while inner_index < 7 do
         if directors_database[row_index][:name] == "Stephen Spielberg"
         puts directors_database[0][:movies][inner_index][:title]
         end
